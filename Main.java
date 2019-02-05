535e977c5bcdaef8f0f0856c9f4dc7c794882793
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-5.local>
2019-02-05 13:21:08 -0500

    Greets only Alice and bob

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        if(name.toLowerCase().equals("alice") || name.toLowerCase().equals("bob")){
+            System.out.printf("Hello %s !! Welcome to ZipCode!!\n", name);
+        }
     }
 }
