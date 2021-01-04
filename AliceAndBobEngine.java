8d9965da647b949c2f9bc53411a969ac119f955b
'nortonlyr' <'nortonlyr@gmail.com'>
2021-01-04 14:04:58 -0500

    First Test Case

---
+++ AliceAndBobEngine.java
@@ -0,0 +1,23 @@
+
+public class AliceAndBobEngine {
+	
+	String result;
+	
+	AliceAndBobEngine(){
+		result = "";
+	}
+	
+	//HINT: Look at the test to see what is expected for different conditions
+	public String evaluateAliceOrBob(String input) {
+		
+		if (input.equals("Alice")) {
+			return "Hello, Alice";
+			
+		} else if (input.equals("Bob")) {
+			return "Hello, Bob";
+			
+		} else {
+			return "I don't know you";
+		}
+	}	
+}
---
+++ AliceAndBobEngineTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+class AliceAndBobEngineTest {
+
+	public AliceBobEngineTest() {
+		
+	}
+	
+	@Before 
+	public void setup() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	void evaluateAliceOrBobTestAlice() {
+		AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
+		
+		assertEquals("Hello, Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
+		assertNotEquals("Hello, Alice", aliceAndBobEngineTest.evaluateAliceOrBob("You are reaching the wrong person"));
+		
+		assertEquals("I don't know you", aliceAndBobEngineTest.evaluateAliceOrBob("Alicia"));
+		assertNotEquals("I don't know you", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
+	}
+	
+	
+
+}
